93d0dbd5422676b42f473b8fcc824dce55049aa9
Hypirius <zuhayerhossain25@gmail.com>
2025-05-13 15:35:34 +0600

    Add Initial grid and create working state

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const container = document.querySelector(".container");
-console.log(container);
-const rect = container.getBoundingClientRect();
-console.log(rect.width);
+const createGrid = document.querySelector("#grid-button");
+
+createGrid.addEventListener("click", function () {
+  const userInput = Number(prompt("Enter a grid size between 2 and 64"));
+
+  if (!userInput) {
+    alert("Enter a valid number");
+    return;
+  }
+  removeBlock();
+  addBlocks(userInput);
+});
+
+function removeBlock() {
+  container.innerHTML = "";
+}
+
+addBlocks(16);
 
 function createBlocks(blockSize) {
   const block = document.createElement("div");
+  const rect = container.getBoundingClientRect();
+  console.log(rect);
   block.style.width = `${(rect.width - 4) / blockSize}px`;
   block.style.height = `${(rect.height - 4) / blockSize}px`;
+  block.classList.add("pixel");
   container.append(block);
 }
 
@@ -17,6 +35,24 @@ function addBlocks(gridSize) {
   addBlockHover();
 }
 
-addBlocks(2);
+function addBlockHover() {
+  container.addEventListener("mouseover", function (event) {
+    const target = event.target;
+    if (target.classList.contains("pixel") && !target.style.backgroundColor) {
+      target.style.backgroundColor = generateRandomColor();
+      target.style.opacity = "1";
+    } else if (target.style.backgroundColor) {
+      if (target.style.opacity > 0) {
+        target.style.opacity = `${target.style.opacity - 0.1}`;
+      }
+    }
+  });
+}
 
-function addBlockHover() {}
+function generateRandomColor() {
+  return `rgb(${createRandomRGB()}, ${createRandomRGB()}, ${createRandomRGB()})`;
+}
+
+function createRandomRGB() {
+  return Math.round(Math.random() * 255);
+}
